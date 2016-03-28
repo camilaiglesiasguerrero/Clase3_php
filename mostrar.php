0c6a1fd9153615912db66491ccc48dd3139e7a06
unknown <camila.ig08@gmail.com>
2016-03-28 20:09:01 -0300

    Incluyo html segun este bien loggueado o no

--- mostrar.php
+++ mostrar.php
@@ -4,9 +4,18 @@ echo "Recibidos: <br>";
 var_dump($_POST);
 //var_dump($_POST);
 echo "<br>";
-$nombre = $_POST['usuario'];
+$nombre = $_POST['usuario']; // => que puedo tomar una variable del array, asignarsela a una variable y mostrarla. O bien puedo trabajar directamente desde el itemd del array
 echo $nombre;
+echo $_POST['password'];
 
+if($nombre=="admin" && $_POST['password'] == "1234")
+{
+	include"bienvenida.html";
+}
+else
+{
+	include"no_loggueado.html";
+}
 
 
  ?>
