8a90fecaaa91f43060574740ac0a451f176f1907
unknown <camila.ig08@gmail.com>
2016-03-28 19:45:19 -0300

    _post // _get

--- mostrar.php
+++ mostrar.php
@@ -1,7 +1,12 @@
 <h1>DATOS RECIBIDOS</h1>
 <?php 
 echo "Recibidos: <br>";
-var_dump($_GET);
+var_dump($_POST);
+//var_dump($_POST);
+echo "<br>";
+$nombre = $_POST['usuario'];
+echo $nombre;
+
 
 
  ?>
